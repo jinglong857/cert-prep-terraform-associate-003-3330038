a10670a116e1bf83d6ae2d736dd0a9d494830830
jinglong857 <jinglong.857@gmail.com>
2026-03-17 23:47:39 +1100

    change instances

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
  resource "aws_instance" "ec2-instance1" {
    ami = "ami-02dfbd4ff395f2a1b"
-   instance_type = "t2.micro"
+   instance_type = "c7i-flex.large"
 
    tags = {                                
      Name = "demo-server1"
@@ -11,7 +11,7 @@
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
   ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
-  instance_type = "t2.micro"
+  instance_type = "c7i-flex.large"
 
   tags = {                                
     Name = "demo-server2"
