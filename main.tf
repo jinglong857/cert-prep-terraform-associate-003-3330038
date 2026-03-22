9a0c3d4a90db59de729715d7655b828548faf80d
jinglong857 <jinglong.857@gmail.com>
2026-03-22 17:51:21 +1100

    change to instance to t3 micro

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "example-db-instance" {
   storage_type = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.t3.micro"
   username             = "admin"
   password             = var.database_password
   parameter_group_name = "default.mysql5.7"
